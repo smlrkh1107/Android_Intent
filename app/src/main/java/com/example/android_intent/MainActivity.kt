8c627cc85ef16b8f4744191711b8c977beea4e1b
smlrkh1107 <krae7789230@gmail.com>
2020-04-12 21:25:52 +0900

    입력한 번호로 전화걸기

--- app/src/main/java/com/example/android_intent/MainActivity.kt
+++ app/src/main/java/com/example/android_intent/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.android_intent
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
     }
+
+    fun setupEvents() {
+        dialBtn.setOnClickListener {
+            val inputPhoneNum = yourPhoneNum.text.toString()
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+        }
+
+
+    }
+
 }
